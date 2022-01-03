d01460fe40e3494b533cbe69e543b62ba1449f33
DESKTOP-Q2GLH75\Le Huy <nghiemlehuy2712@gmail.com>
2022-01-03 14:11:24 +0700

    improve cat eat

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var Dog=require('./Dog');
 var Cat=require('./Cat');
 var Cat2=require('./Cat')
+var Mouse=require('./Mouse');
 var dog=new Dog('Tom');
 var tom=new Cat();
 dog.eat(tom);
 console.log(dog);
+var cat=new Cat();
+cat.eat(mouse);
